b664533e473f20ccb43e4a48c4db8d6d1e6a6c4d
jaikeex <jaikeex@users.noreply.github.com>
2021-08-24 14:32:55 +0200

    Added more controller tests

--- src/test/java/com/jaikeex/mywebpage/controllers/UserControllerTest.java
+++ src/test/java/com/jaikeex/mywebpage/controllers/UserControllerTest.java
@@ -4,42 +4,41 @@ import com.jaikeex.mywebpage.dto.UserDto;
 import com.jaikeex.mywebpage.entity.User;
 import com.jaikeex.mywebpage.jpa.UserRepository;
 import com.jaikeex.mywebpage.services.UserAccountManagementService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    UserController controller;
-
-    @MockBean
-    UserAccountManagementService service;
-
     @MockBean
     UserRepository repository;
-
-    String TOKEN_ATTR_NAME = "org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN";
-    HttpSessionCsrfTokenRepository tokenRepository = new HttpSessionCsrfTokenRepository();
-    CsrfToken token = tokenRepository.generateToken(new MockHttpServletRequest());
-
+    @MockBean
+    UserAccountManagementService service;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
 
     private final UserDto userDto = new UserDto(
             "testuserfordbaccess@testuserfordbaccess.com",
@@ -59,20 +58,40 @@ class UserControllerTest {
             true,
             "USER");
 
-
-    @Test
-    public void ContextLoads() {
-        assertNotNull(controller);
+    @BeforeEach
+    public void beforeEach() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+        when(service.registerUser(any(UserDto.class), any(HttpServletRequest.class), any(Model.class))).thenReturn(testUser1);
     }
 
     @Test
-    public void registerUserWithNoErrors() throws Exception {
-        this.mockMvc.perform(get("/")).andExpect(status().isOk());
+    public void shouldReturnWarningThatFormIsNotFilled() throws Exception {
+        mockMvc.perform(post("/user/signup").with(csrf())).andExpect(status().isOk()).andExpect(content().string(containsString("Please fill in all the fields!")));
     }
 
     @Test
-    public void shouldReturnIndexWelcomeMessage() throws Exception {
-        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(containsString("Welcome")));
+    public void shouldRegisterUserAndLogHimIn() throws Exception {
+        mockMvc.perform(post("/user/signup").with(csrf()).with(user("user").password("pass").roles("USER")))
+                .andExpect(status().isOk()).andExpect(content().string(containsString("Registration was successful")));
     }
 
+    @Test
+    public void shouldFailWithInvalidEmail() throws Exception {
+        userDto.setEmail("testuserfordbaccess@testuserfordbaccess");
+        mockMvc.perform(post("/user/signup").with(csrf()).flashAttr("userDto", userDto))
+                .andExpect(status().isOk()).andExpect(content().string(containsString("Wrong email")));
+    }
+
+    @Test
+    public void shouldFailWithNoMatchingPassword() throws Exception {
+        userDto.setPasswordForValidation("testuserfordbaccess123");
+        mockMvc.perform(post("/user/signup").with(csrf()).flashAttr("userDto", userDto))
+                .andExpect(status().isOk()).andExpect(content().string(containsString("The passwords")));
+    }
+
+    @Test
+    public void userDtoIsCreatedInGetToSignup() throws Exception {
+        mockMvc.perform(get("/user/signup"))
+                .andExpect(status().isOk()).andExpect(model().attributeExists("userDto"));
+    }
 }
