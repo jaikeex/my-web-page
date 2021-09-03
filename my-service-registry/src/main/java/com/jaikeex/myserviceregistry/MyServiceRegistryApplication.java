953475d2d1458d7a0af155e0b9921f489a497efb
jaikeex <jaikeex@users.noreply.github.com>
2021-09-03 12:05:39 +0200

    Created Projects service

---
+++ my-service-registry/src/main/java/com/jaikeex/myserviceregistry/MyServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.jaikeex.myserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServiceRegistryApplication.class, args);
+	}
+
+}
