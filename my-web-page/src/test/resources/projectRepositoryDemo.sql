15129c96dfd0099ec602feb9a2175bd386f40033
jaikeex <jaikeex@users.noreply.github.com>
2021-09-03 11:49:28 +0200

    Created new directory from all msc will be refactored

---
+++ my-web-page/src/test/resources/projectRepositoryDemo.sql
@@ -0,0 +1,12 @@
+#INSERT INTO PROJECT (name, github_link, introduction, detailed_description, language, snapshot)
+#VALUES ('some project', NULL, NULL, NULL, "Java", NULL);
+#INSERT INTO TECHNOLOGY (name)
+#VALUES ('spring');
+#INSERT INTO TECHNOLOGY (name)
+#VALUES ('jpa');
+
+
+
+
+
+
